3f9f5a8faa908c9f7049b1ff43e6aaefdd66a6aa
Giannicola Olivadoti <olinicola@gmail.com>
2012-02-18 18:24:06 +0100

    Added Librarian gem for cookbooks handling

--- chef/roles/base.rb
+++ chef/roles/base.rb
@@ -1,5 +1,18 @@
 name "base"
 description "Base role applied to all nodes."
 run_list(
-  "recipe[test]"
+  "recipe[apt]"
+  # "recipe[iptables]"
+  # "recipe[openssh]",
+  # "recipe[zsh]",
+  # "recipe[sudo]",
+  # "recipe[ntp]",
+  # "recipe[build-essential]",
+  # "recipe[vim]",
+  # "recipe[git]",
+  # "recipe[ssh_known_hosts]",
+  # "recipe[screen]",
+  # "recipe[hostname]",
+  # "recipe[monit]",
+  # "recipe[fail2ban]"
 )
