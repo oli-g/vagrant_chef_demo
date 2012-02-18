2a7a1546fbb82265a6d4d643fc1c9261376ce782
Giannicola Olivadoti <olinicola@gmail.com>
2012-02-18 16:57:24 +0100

    Initial commit: used Vagrant with Chef

---
+++ chef/cookbooks/test/recipes/default.rb
@@ -0,0 +1,5 @@
+directory "/tmp/foobarbaz" do
+  owner "root"
+  group "root"
+  mode 0755
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VagrantChefDemo::Application.config.secret_token = 'd1beacf65876f3661ceda9f7ad7629b3aa0e8996dc51f4fc864f04b85a2404241be51682081b675c7141957bfd7869fb2a41e20576faeef43fef3a3ea1906562'
